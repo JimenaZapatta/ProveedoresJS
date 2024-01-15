893ea56d460aedaaedb0f89b90912f69c6639e75
JimenaZapatta <jimez03@gmail.com>
2024-01-15 00:21:04 -0300

    guardar js

---
+++ consulta.js
@@ -0,0 +1,16 @@
+let razonsocial=prompt("Ingrese Razón Social");
+
+if(razonsocial == "") {
+    alert( "Error: Favor ingresar su Razón Social");}
+    
+else  { alert("Bienvenido" + " "+ razonsocial);}
+
+const ListaPagos=["Ecoquimica"," Mircoslues", "Larrique","Granelur", "Bioterra"," Linn"," Bako"]
+console.log (ListaPagos);
+console.log (ListaPagos.includes(razonsocial));
+
+let resultado= ListaPagos.includes(razonsocial)
+if(resultado == true){
+    alert("Pueden pasar a cobrar el martes desde 8:30 a 12:00, gracias.");
+}
+else {alert(razonsocial +":"+ " "+ "No hay pago para dicha empresa, consutar nuevamente el jueves que viene.")}
