9208ef08c6fe1a8e72cedc22b8035ed4814680cf
JimenaZapatta <jimez03@gmail.com>
2024-01-28 22:11:35 -0300

    ingresar

--- consulta.js
+++ consulta.js
@@ -1,20 +1,24 @@
-
-function filtrelistadepagos (){
-let razonsocial = prompt ("Ingrese razón social").toLowerCase().trim(); {
-
-if(razonsocial == "") {
-    alert( "Error: Favor ingresar su Razón Social")}
-    
-else { alert("Bienvenido" + " "+ razonsocial);}}
-
-const ListaPagos=["Bioterra"," Mircoslues", "Larrique","Granelur", "Bioterra"," Linn"," Bako"]
-console.log (ListaPagos.includes(razonsocial));
-console.log (ListaPagos);
-
-
-let resultado= ListaPagos.filter((x)=> x.razonsocial.toUpperCase().trim().includes(razonsocial))
-
-if(resultado.length>0){
-    alert("Pueden pasar a cobrar el martes desde 8:30 a 12:00, gracias.");
+const listapagos= function (Rut, Razónsocial){
+this.Rut= Rut
+this.Razónsocial=Razónsocial
 }
-else {alert(razonsocial +":"+ " "+ "No hay pago para dicha empresa, consultar nuevamente el jueves que viene.")}}
+let prov1 = new listapagos (123, "Bioterra")
+let prov2 = new listapagos (124, "Sudel")
+let prov3 = new listapagos (125, "Granelur")
+let prov4 = new listapagos (126, "Ijasa")
+let prov5 = new listapagos (127, "Tor")
+
+let lista = [prov1,prov2,prov3,prov4,prov5]
+
+function consultars(){   
+let ingresers= prompt("Ingrese Razón Social").toLowerCase().trim(); 
+let resultado = lista.filter((listapagos)=>listapagos.Razónsocial.toLowerCase().trim())
+if(resultado.length>0){
+alert("puede pasar este viernes en el horario de 8:30 a 12:00")
+}else{alert("no tiene pago para este Viernes, vuelva a intetar la semana que viene")}
+
+if (ingresers== ""){
+    alert ("favor ingresa razón social");
+}else { (alert ("Bienvenido" +" "+ ingresers))}
+}
+
