049dd5ad7eec87e6190d40485fd400c82e2dd594
JimenaZapatta <jimez03@gmail.com>
2024-01-28 22:36:34 -0300

    guardar

--- consulta.js
+++ consulta.js
@@ -8,17 +8,23 @@ let prov3 = new listapagos (125, "Granelur")
 let prov4 = new listapagos (126, "Ijasa")
 let prov5 = new listapagos (127, "Tor")
 
-let lista = [prov1,prov2,prov3,prov4,prov5]
-
-function consultars(){   
+let lista = [prov1,prov2,prov3,prov4,prov5];
+ 
 let ingresers= prompt("Ingrese Razón Social").toLowerCase().trim(); 
-let resultado = lista.filter((listapagos)=>listapagos.Razónsocial.toLowerCase().trim())
-if(resultado.length>0){
-alert("puede pasar este viernes en el horario de 8:30 a 12:00")
-}else{alert("no tiene pago para este Viernes, vuelva a intetar la semana que viene")}
 
 if (ingresers== ""){
+
     alert ("favor ingresa razón social");
+for (break;)
+
 }else { (alert ("Bienvenido" +" "+ ingresers))}
-}
+
+
+let resultado = lista.filter((listapagos)=>listapagos.Razónsocial.toLowerCase().trim())
+if(resultado.length<0){
+alert("puede pasar este viernes en el horario de 8:30 a 12:00")
+
+}else{alert("no tiene pago para este Viernes, vuelva a intetar la semana que viene")}
+
+
 
