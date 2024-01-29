5b1610a2370ee92674ebb48f6432fb1de6b9bbd9
JimenaZapatta <jimez03@gmail.com>
2024-01-28 21:09:03 -0300

    guardar

--- consulta.js
+++ consulta.js
@@ -1,16 +1,20 @@
-let razonsocial=prompt("Ingrese Razón Social");
+
+function filtrelistadepagos (){
+let razonsocial = prompt ("Ingrese razón social").toLowerCase().trim(); {
 
 if(razonsocial == "") {
-    alert( "Error: Favor ingresar su Razón Social");}
+    alert( "Error: Favor ingresar su Razón Social")}
     
-else  { alert("Bienvenido" + " "+ razonsocial);}
+else { alert("Bienvenido" + " "+ razonsocial);}}
 
-const ListaPagos=["Ecoquimica"," Mircoslues", "Larrique","Granelur", "Bioterra"," Linn"," Bako"]
-console.log (ListaPagos);
+const ListaPagos=["Bioterra"," Mircoslues", "Larrique","Granelur", "Bioterra"," Linn"," Bako"]
 console.log (ListaPagos.includes(razonsocial));
+console.log (ListaPagos);
 
-let resultado= ListaPagos.includes(razonsocial)
-if(resultado == true){
+
+let resultado= ListaPagos.filter((x)=> x.razonsocial.toUpperCase().trim().includes(razonsocial))
+
+if(resultado.length>0){
     alert("Pueden pasar a cobrar el martes desde 8:30 a 12:00, gracias.");
 }
-else {alert(razonsocial +":"+ " "+ "No hay pago para dicha empresa, consultar nuevamente el jueves que viene.")}
+else {alert(razonsocial +":"+ " "+ "No hay pago para dicha empresa, consultar nuevamente el jueves que viene.")}}
