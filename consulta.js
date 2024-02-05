b2bf9771e919ce63e26f67b61e7b9477843ea054
JimenaZapatta <jimez03@gmail.com>
2024-02-05 19:30:52 -0300

    guardar

--- consulta.js
+++ consulta.js
@@ -11,10 +11,11 @@ let prov5 = new listapagos (127, "Tor")
 let lista = [prov1,prov2,prov3,prov4,prov5];
 
 const Buscar = document.getElementById ("buscar");
-const buscador= document.querySelector("#buscador");
-const result1 = document.getElementById("ingrut").value;
+const mostrarbusqueda= document.querySelector("#mostrarbusqueda");
+
 Buscar.addEventListener ("click", function(){
-        
+
+    const result1 = document.getElementById("ingrut").value;        
         const pagoencontrado = lista.find(item => item.Rut === parseInt(result1));
         if( result1 === "") {
             alert("Ingresar Rut");
@@ -25,8 +26,8 @@ Buscar.addEventListener ("click", function(){
         
         if (pagoencontrado) {
             const pagoviernes = document.createElement("div");
-            pagoviernes.innerHTML =`<p>$${pagoencontrado.Razónsocial} puede pasar el viernes desde 8:30 a 12:00</p>`;
-            buscador.appendChild(pagoviernes);
+            pagoviernes.innerHTML =`<p>${pagoencontrado.Razónsocial} puede pasar el viernes desde 8:30 a 12:00</p>`;
+            mostrarbusqueda.appendChild(pagoviernes);
         } else {
             alert("No se encontró ningún pago.");
         }
